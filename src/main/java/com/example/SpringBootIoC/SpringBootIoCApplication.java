40f67af6f52c7e00c97bf433bea6714e356277d7
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-08-25 17:25:24 -0600

    Inyectando el bean en la clase principal

--- src/main/java/com/example/SpringBootIoC/SpringBootIoCApplication.java
+++ src/main/java/com/example/SpringBootIoC/SpringBootIoCApplication.java
@@ -1,13 +1,18 @@
 package com.example.SpringBootIoC;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.example.SpringBootIoC.repository.ProductRepository;
+
 @SpringBootApplication
 public class SpringBootIoCApplication implements CommandLineRunner  {
 
+	@Autowired
+	private ProductRepository productRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootIoCApplication.class, args);
 	}
@@ -19,8 +24,16 @@ public class SpringBootIoCApplication implements CommandLineRunner  {
 
 	@Bean
 	CommandLineRunner comandoGenerico(){
-	return args -> {
-		System.out.println("Hola desde la interfaz CommandLineRunner implementada con una expresion lambda");
-	};
+		return args -> {
+			System.out.println("Hola desde la interfaz CommandLineRunner implementada con una expresion lambda");
+		};
+	}
+
+	@Bean
+	CommandLineRunner commandoUtilizandoAutowired(){
+		return args -> {
+			productRepository.save("Control remoto");
+			productRepository.remove("Mouse USB generico");
+		};
 	}
 }
