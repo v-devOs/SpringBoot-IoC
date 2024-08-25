0f7c7678129b04c28f24e389e0f8967a6c0209ef
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-08-25 17:12:02 -0600

    Interfaz CommandLineRunner

--- src/main/java/com/example/SpringBootIoC/SpringBootIoCApplication.java
+++ src/main/java/com/example/SpringBootIoC/SpringBootIoCApplication.java
@@ -1,13 +1,30 @@
 package com.example.SpringBootIoC;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringBootIoCApplication {
+public class SpringBootIoCApplication implements CommandLineRunner  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootIoCApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hola desde la interfaz CommandLineRunner");
+
+	}
+
+	@Bean
+	CommandLineRunner comandoGenerico(){
+	return args -> {
+		System.out.println("Hola desde la interfaz CommandLineRunner implementada con una expresion lambda");
+	};
+	}
+
+
+	
 }
