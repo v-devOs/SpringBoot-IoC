88a0af5be8ac19a49a5a11b4bb3ebfd0e21c46c5
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-08-25 17:03:24 -0600

    Initial commit

---
+++ src/main/java/com/example/SpringBootIoC/SpringBootIoCApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootIoC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIoCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootIoC/SpringBootIoCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootIoC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIoCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
